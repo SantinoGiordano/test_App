3b310237d9ba85ea2f7ac80e62b0192dbb1dd7b4
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-24 15:20:30 -0500

    removed fonts and added my own

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Nav from "./componets/Nav";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
+
+const cutOuts = localFont({
+  src:"./fonts/CutOutsFLF.ttf",
+  weight: "400",
+  variable: "--font-cutouts"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+      <header >
+      <Nav/>
+      </header>
         {children}
       </body>
     </html>
