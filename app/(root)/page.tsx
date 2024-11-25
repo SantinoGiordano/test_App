14cd6d6bbe14f8eb12dca02b30a30ef53feba7c6
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-25 18:29:36 -0500

    changed hello world  heading

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+
+// import style from '@/app/page.module.css'
+
+
+export default function Home() {
+
+  return (
+    <>
+      <div className='text-center text-4xl font-'>
+        hello world
+      </div>
+    </>
+  );
+}
