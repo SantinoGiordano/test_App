26d157e385127989b9515654fad062311381c688
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-25 18:46:37 -0500

    created blured backdrop nav

--- app/componets/Nav.tsx
+++ app/componets/Nav.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
 import React from "react";
 
-
 const Nav = () => {
   return (
     <>
-      <div className='text-right text-xl text-white p-10 bg-slate-500'>
+      <div className=" backdrop-blur-md bg-opacity-50 text-right text-xl text-black p-10 bg-slate-200 fixed top-0 w-full">
         <Link className="p-5" href={"./"}>
           Home
         </Link>
-        <Link className="p-5 " href={"./product"}>
-          Product
+        <Link className="p-5 " href={"./events"}>
+          Events
         </Link>
         <Link className="p-5" href={"./sale"}>
           Sales
