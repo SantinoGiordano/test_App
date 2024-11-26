78b49f59970440af8c67b8fd87c6c24b09abe76d
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-26 11:56:54 -0500

    temp text event card later will be event.map and data will be passed in

---
+++ app/componets/EventCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const EventCard = () => {
+  return (
+    <>
+    <div className='eventCard'>
+      <div className='eventCard-title'>
+        Title: Party
+      </div>
+      <div>
+      Join us for an unforgettable night of music and energy at Harmony Live 2024! 
+      Featuring chart-topping artists and local talents, 
+      this concert promises an electrifying atmosphere with stunning light shows, 
+      incredible performances, and surprises at every turn. 
+      Whether you're a die-hard music fan or just looking for a night of fun,
+       this event is perfect for all ages. 
+       Grab your tickets now and be part of a celebration that will leave you singing and dancing long after the final encore!
+      </div>
+    </div>
+    </>
+
+  )
+}
+
+export default EventCard
