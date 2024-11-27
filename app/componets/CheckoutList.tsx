15bb2592d5b2fa0b127febef581129feb402699a
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-27 17:04:24 -0500

    CheckList item basic setup

---
+++ app/componets/CheckoutList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CheckoutList = () => {
+  return (
+        <div className='p-10 bg-slate-50 text-black flex-col fixed right-4 top-4 mt-40 rounded border-blue-700 border-[3px] text-right'>
+            Checkout:<hr className='h-[3px] bg-blue-500 border-none'/>
+        </div>
+
+  )
+}
+
+export default CheckoutList
