cb4455df25ad3e4d728f34014f044e287280a2fe
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-27 18:17:47 -0500

    changed up checklist

--- app/componets/CheckoutList.tsx
+++ app/componets/CheckoutList.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const CheckoutList = () => {
-  return (
-        <div className='p-10 bg-slate-50 text-black flex-col fixed right-4 top-4 mt-40 rounded border-blue-700 border-[3px] text-right'>
-            Checkout:<hr className='h-[3px] bg-blue-500 border-none'/>
-        </div>
-
-  )
+interface CheckoutListProps {
+  items: string[];
 }
 
-export default CheckoutList
+const CheckoutList: React.FC<CheckoutListProps> = ({ items }) => {
+  return (
+    <div className="p-10 bg-slate-50 text-black flex-col fixed right-4 top-4 mt-40 rounded border-blue-700 border-[3px] text-right">
+      <div>
+        Checkout:
+        <hr className="h-[3px] bg-blue-500 border-none" />
+        <ul className="list-disc list-inside">
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutList;
