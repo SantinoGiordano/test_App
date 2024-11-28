cc5cd4014fbfeaa7f8dfb1b9ac60187e13ecb5e9
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-27 20:08:41 -0500

    boilerplate code for about

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return (
+  
+  <>
+
+    <div className='flex row-span-2'>
+      <div className='mt-40 p-5'>
+        hello world
+      </div>
+      <div className='mt-40 p-5'>
+        hello world
+        </div>
+    </div>
+  
+  </>
+  )
+}
+
+export default About
