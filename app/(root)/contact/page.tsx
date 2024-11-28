7a01594e82fbef58c4901bd1e6b05a4dff261eae
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-27 20:09:07 -0500

    boilerplate code for contact

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Contact</div>
+  
+  <>
+
+    <div className='flex row-span-2'>
+      <div className='mt-40 p-5'>
+        hello contact
+      </div>
+      <div className='mt-40 p-5'>
+        hello contact
+        </div>
+    </div>
+  
+  </>
   )
 }
 
