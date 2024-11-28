4556b0fbcad2e2601390f2a42d2bed6043b050d4
SantinoGiordano <santinogiordano13@gmail.com>
2024-11-27 20:47:48 -0500

    about bg and contact / start up screen

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-  
-  <>
-
-    <div className='flex row-span-2'>
-      <div className='mt-40 p-5'>
-        hello world
-      </div>
-      <div className='mt-40 p-5'>
-        hello world
+    <div className="bg-slate-600">
+      <div className="h-screen bg-fixed bg-center bg-cover bg-background-image-home">
+        <div className="h-full bg-black/50 flex items-center justify-center">
+          <h1 className="text-white text-4xl md:text-6xl font-bold">
+            Parallax Effect
+          </h1>
         </div>
+      </div>
+      <div className="flex row-span-2 pt-40">
+        <img src="wedding.jpg" className="w-2/4" />
+        <div className="mt-40 p-5">hello world</div>
+      </div>
     </div>
-  
-  </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -1,21 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-  
-  <>
-
-    <div className='flex row-span-2'>
-      <div className='mt-40 p-5'>
-        hello contact
-      </div>
-      <div className='mt-40 p-5'>
-        hello contact
+    <>
+      <div className="bg-slate-600">
+        <div className="flex row-span-2">
+          <div className="mt-40 p-5">hello contact</div>
+          <div className="mt-40 p-5">hello contact</div>
         </div>
-    </div>
-  
-  </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default page
+export default page;
